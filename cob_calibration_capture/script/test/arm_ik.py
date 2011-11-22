b2b9b0cbe88608dbd22346f4a958942e27e051e9
ipa-fmw-sh <sebhaug@gmail.com>
2011-11-22 14:40:01 +0100

    improved seed to get current angles

--- cob_calibration_capture/script/test/arm_ik.py
+++ cob_calibration_capture/script/test/arm_ik.py
@@ -1,19 +1,16 @@
 #!/usr/bin/env python
 PKG  = 'cob_calibration_capture'
-NODE = 'move_arm'
+NODE = 'arm_ik_node'
 import roslib; roslib.load_manifest(PKG)
 import rospy
 
 from simple_script_server import simple_script_server
-from kinematics_msgs.srv import *
+from kinematics_msgs.srv import GetPositionIK, GetPositionIKRequest
+from sensor_msgs.msg import JointState
 from geometry_msgs.msg import PoseStamped
 import tf
 
-def getIk(arm_ik, tf_listener, goal_pose, link):
-    # get seed joint state (pregrasp assumend here)
-    arm_pre_grasp = rospy.get_param("/script_server/arm/pregrasp")
-    print arm_pre_grasp[0]
-    
+def getIk(arm_ik, (t, q), link):
     # get joint names for arm from parameter server
     joint_names = None
     try: joint_names = rospy.get_param("arm_controller/joint_names") # real hardware
@@ -21,24 +18,42 @@ def getIk(arm_ik, tf_listener, goal_pose, link):
     try: joint_names = rospy.get_param("arm_controller/joints")      # simulation
     except KeyError: pass
     if joint_names == None:
-        print "Could not get joint names from parameter server. exiting..."
+        print "Could not get arm joint_names from parameter server. exiting..."
         exit(-1)
+        
+    # get current joint angles from arm as seed position
+    current_joint_angles = []
+    for tries in range(10):
+        try: msg = rospy.wait_for_message("/joint_states", JointState)
+        except rospy.exceptions.ROSInterruptException: pass
+        if joint_names[0] in msg.name:
+            # message is from arm, save current angles
+            for name in joint_names: current_joint_angles.append(msg.position[msg.name.index(name)])
+            break
+    if current_joint_angles == []:
+        print "Could not get /joint_states message from arm controller. Assuming zero seed state..."
+        for name in joint_names: current_joint_angles.append(0.0)
     
     # create and send ik request
     req = GetPositionIKRequest()
     req.timeout = rospy.Duration(0.5)
     req.ik_request.ik_link_name = "arm_7_link"
-    req.ik_request.ik_seed_state.joint_state.position = arm_pre_grasp[0]
+    req.ik_request.ik_seed_state.joint_state.position = current_joint_angles
     req.ik_request.ik_seed_state.joint_state.name = joint_names
     req.ik_request.pose_stamped.header.frame_id = link
-    req.ik_request.pose_stamped.pose.position = goal_pose.pose.position
-    req.ik_request.pose_stamped.pose.orientation = goal_pose.pose.orientation
+    req.ik_request.pose_stamped.pose.position.x = t[0]
+    req.ik_request.pose_stamped.pose.position.y = t[1]
+    req.ik_request.pose_stamped.pose.position.z = t[2]
+    req.ik_request.pose_stamped.pose.orientation.x = q[0]
+    req.ik_request.pose_stamped.pose.orientation.y = q[1]
+    req.ik_request.pose_stamped.pose.orientation.z = q[2]
+    req.ik_request.pose_stamped.pose.orientation.w = q[3]
     resp = arm_ik(req)
     
     if resp.error_code.val == resp.error_code.SUCCESS:
         result = resp.solution.joint_state.position
     else:
-        print "Inverse kinematics failed"
+        print "Inverse kinematics request failed"
         result = None
     return result
 
@@ -56,21 +71,10 @@ def main():
     # get current pose of arm
     tf_listener.waitForTransform("base_link", "arm_7_link", rospy.Time(), rospy.Duration(1))
     (t, q) = tf_listener.lookupTransform("base_link", "arm_7_link", rospy.Time())
-    #print (t, q)
     
-    # create goal pose and query ik server
-    goal_pose = PoseStamped()
-    goal_pose.header.frame_id = "base_link"
-    # move 1cm in x direction
-    goal_pose.pose.position.x = t[0]+0.1
-    goal_pose.pose.position.y = t[1]-0.1
-    goal_pose.pose.position.z = t[2]-0.1
-    goal_pose.pose.orientation.x = q[0]
-    goal_pose.pose.orientation.y = q[1]
-    goal_pose.pose.orientation.z = q[2]
-    goal_pose.pose.orientation.w = q[3]
-    #print goal_pose
-    print getIk(arm_ik, tf_listener, goal_pose ,"base_link")
+    print "--> calling getIk"
+    print getIk(arm_ik, (t, q) ,"base_link")
+    print getIk(arm_ik, (t+(0.1, 0.5, -0.2), q) ,"base_link")
 
 if __name__ == '__main__':
     main()
