51bd18e9a432fa26885eca6dcc9b901c1097be41
ipa-fmw-sh <sebhaug@gmail.com>
2011-12-06 21:01:10 +0100

    move arm to all stereo pos

---
+++ cob_calibration_capture/script/test/move_arm_all_stereo.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+PKG  = 'cob_calibration_capture'
+NODE = 'move_arm'
+import roslib; roslib.load_manifest(PKG)
+import rospy
+
+from simple_script_server import simple_script_server
+
+# main
+def main():
+    rospy.init_node(NODE)
+    print "==> started " + NODE
+    
+    # init
+    sss = simple_script_server()
+
+    # movements
+    print "==> starting movements"
+    
+    stereo = rospy.get_param("/script_server/arm/stereo")
+    print stereo
+
+    for pos in stereo:
+        print "--> moving arm to %s" % pos
+        sss.move("arm", pos)
+   
+if __name__ == '__main__':
+    main()
+    print "==> done exiting"
