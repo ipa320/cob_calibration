3eba9ea4db8e3415d1474f1ab6979e4647b9e699
ipa-fmw-sh <sebhaug@gmail.com>
2011-12-08 21:31:47 +0100

    calib positions changed and move arm extended

--- cob_calibration_capture/script/test/move_arm.py
+++ cob_calibration_capture/script/test/move_arm.py
@@ -3,14 +3,22 @@ PKG  = 'cob_calibration_capture'
 NODE = 'move_arm'
 import roslib; roslib.load_manifest(PKG)
 import rospy
+import sys
 
 from simple_script_server import simple_script_server
 
 # main
-def main():
+def main(args):
     rospy.init_node(NODE)
     print "==> started " + NODE
-    
+
+    # extract args
+    pos = None
+    allowed_pos = ["calib", "hand_eye", "stereo"]
+    if len(args) > 0:
+	if args[0] in allowed_pos: pos = args[0]
+    if pos == None: pos = "calib"
+
     # init
     sss = simple_script_server()
 
@@ -20,12 +28,10 @@ def main():
     print "--> moving arm to pregrasp"
     sss.move("arm", "pregrasp")
     
-    print "--> moving arm to calib"
-    sss.move("arm", "calib")
-    
-#    print "--> moving arm to all stereo"
-#    sss.move("arm", "stereo")
+    print "--> moving arm to %s", pos
+    sss.move("arm", pos)
     
 if __name__ == '__main__':
-    main()
+    args = sys.argv
+    main(args[1:])
     print "==> done exiting"
