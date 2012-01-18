3570d27a3aac2caa3a79eb82f8c714a5e3fd4200
ipa-fmw-sh <sebhaug@gmail.com>
2012-01-18 22:58:40 +0100

    yaml file compare improved

--- cob_robot_calibration/test/test_camera_yaml_updater.py
+++ cob_robot_calibration/test/test_camera_yaml_updater.py
@@ -3,6 +3,7 @@ PKG = 'cob_robot_calibration'
 import roslib; roslib.load_manifest(PKG)
 import unittest
 
+import yaml
 from cob_robot_calibration import camera_yaml_updater 
 
 FILE_IN = "test/data/test_camera_cal.yaml"
@@ -17,7 +18,7 @@ class TestCameraYamlUpdater(unittest.TestCase):
         updater.update_baseline(0.0)
         
         # compare with correct results
-        self.assertTrue(self._cmp_files(FILE_OUT, FILE_IN))
+        self.assertTrue(self._cmp_yaml_files(FILE_OUT, FILE_IN))
         
     def test_update_baseline_positive(self):
         # do update
@@ -25,7 +26,7 @@ class TestCameraYamlUpdater(unittest.TestCase):
         updater.update_baseline(10.0)
         
         # compare with correct results
-        self.assertTrue(self._cmp_files(FILE_OUT, FILE_OUT_RES_POS))
+        self.assertTrue(self._cmp_yaml_files(FILE_OUT, FILE_OUT_RES_POS))
 
     def test_update_baseline_negative(self):
         # do update
@@ -33,10 +34,10 @@ class TestCameraYamlUpdater(unittest.TestCase):
         updater.update_baseline(-10.0)
         
         # compare with correct results
-        self.assertTrue(self._cmp_files(FILE_OUT, FILE_OUT_RES_NEG))
+        self.assertTrue(self._cmp_yaml_files(FILE_OUT, FILE_OUT_RES_NEG))
 
-    def _cmp_files(self, f1, f2):
-        return open(f1).read() == open(f2).read()
+    def _cmp_yaml_files(self, f1, f2):
+        return yaml.load(file(f1)) == yaml.load(file(f2))
 
 if __name__ == '__main__':
     import rosunit
