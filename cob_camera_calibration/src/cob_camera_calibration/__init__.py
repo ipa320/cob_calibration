31f10eb532e856fcd8c2c6344daf18353ddc8893
ipa-fmw-sh <sebhaug@gmail.com>
2012-01-23 16:51:39 +0100

    calibration urdf updater added/fixed

--- cob_camera_calibration/src/cob_camera_calibration/__init__.py
+++ cob_camera_calibration/src/cob_camera_calibration/__init__.py
@@ -2,3 +2,4 @@ from calibration_data import CalibrationData
 from calibration_object import Checkerboard
 from calibration_object_detector import CheckerboardDetector
 from calibrator import MonoCalibrator, StereoCalibrator
+from calibration_urdf_updater import CalibrationUrdfUpdater
