d084374b7ca069dfc93133eea7ea9a30710562a3
Jannik Abbenseth <jannik.abbenseth@gmail.com>
2012-12-18 11:37:38 +0100

    generate_config script modified for new sensors.yaml

--- cob_robot_calibration/script/generate_config.py
+++ cob_robot_calibration/script/generate_config.py
@@ -53,8 +53,9 @@
 # If not, see <http://www.gnu.org/licenses/>.
 #
 #################################################################
-PKG  = 'cob_robot_calibration'
-import roslib; roslib.load_manifest(PKG)
+PKG = 'cob_robot_calibration'
+import roslib
+roslib.load_manifest(PKG)
 import rospy
 
 from simple_script_server import simple_script_server
@@ -62,146 +63,157 @@ import yaml
 import tf
 
 
-
-def get_transformation_camera_chain(d,origin):
-    return get_transformation_chain(d['chain'],origin)
-    
-
-def get_transformation_chain(d,origin):
-    transformation=[]
-    transformation.append(origin+d['before_chain'])
-    ll=[d['last_link']]
-    after_chain=d['after_chain']
-    after_chain=ll+after_chain
-    transformation+=[after_chain]
-    to_calibrate=[d['before_chain'][-1],after_chain[-1]]
-    return transformation, to_calibrate
-    
-def prettyprint(l):
-    for x in l:
-        print x
-        
-def get_chains(d,origin=['base_link']):
-    print '[INFO]: %s was selected as root frame'%origin
-    if not isinstance(d,dict):
-        print "[ERROR]: d should be dict, is %s"%type(d)
+def get_chains(d, origin='base_link'):
+    print '[INFO]: %s was selected as root frame' % origin
+    if not isinstance(d, dict):
+        print "[ERROR]: d should be dict, is %s" % type(d)
         return
-    if not isinstance(origin, list):
-        origin=[origin]
-        print "[INFO] origin should be list, is %s. Conversion done"%type(origin)
-    to_calibrate= []
-    transformations=[]
-    for k,v in d.iteritems():
-        if k=='camera_chains':
+    chains = transform_chain_dict(d['chains'])
+    trees = []
+    for k, v in d.iteritems():
+        if k == 'camera_chains':
             for x in v:
-                t,to_calib=get_transformation_camera_chain(x,origin)
-                transformations.append(t)
-                to_calibrate+=to_calib
-        elif k=='chains':
+                trees += build_tree(x['chain'], chains, origin)
+        elif k == 'sensor_chains':
             for x in v:
-                t,to_calib=get_transformation_chain(x,origin)
-                transformations.append(t)
-                to_calibrate+=to_calib
-    to_calibrate=list(set(to_calibrate))
-    
-    return transformations,to_calibrate
+                trees += build_tree(x, chains, origin)
 
-def generate_transformation_dict(transformation_list,listener):
-    transformation_dict={}
-    for t in transformation_list:
-        for chain in t:
-            index_list=range(len(chain))
-            index_list.remove(0)
-            for index in index_list:
-                key=chain[index]
-                if not transformation_dict.has_key(key):
-                    
-                    value=get_single_transform(chain[index-1],chain[index],listener)
-                    transformation_dict[key]=value
+    print " Trees: "
+    print trees
+    #to_calibrate = list(set(to_calibrate))
+    return trees
+    #return transformations, to_calibrate
+
+
+def transform_chain_dict(d):
+    new_dict = {}
+    for chain in d:
+        new_dict[chain['chain_id']] = chain
+    return new_dict
+
+
+def build_tree(chain, kinematic_chains, origin):
+    tree = [origin]
+    tree += [''.join(chain['before_chain'])]
+    tree = cleanup_tree(tree)
+    for chain_id in chain['chains']:
+        kinematic_chain = kinematic_chains[chain_id]
+        try:
+            offset = -1
+            if tree[offset] != kinematic_chain['parent_frame']:
+                print 'There might be an Error in this tree due to undefined parent frame for kinematic chain'
+            if tree[offset] == ''.join(kinematic_chain['before_chain']):
+                tree += kinematic_chain['before_chain'][1:]
+        except IndexError:
+            tree += kinematic_chain['before_chain']
+        tree += [''.join(kinematic_chain['links'][0])]
+        tree += ['**']
+        tree += [''.join(kinematic_chain['links'][1])]
+        tree += [''.join(kinematic_chain['after_chain'])]
+        tree = cleanup_tree(tree)
+    tree += chain['after_chain']
+    tree += ['**']
+    print '*' * 3, ' New Tree ', '*' * 3
+    print(tree)
+
+    return tree
+
+
+def cleanup_tree(tree):
+    new_tree = []
+    for v in tree:
+        if v != '':
+            new_tree.append(v)
+    return new_tree
+
+
+def generate_transformation_dict(transformation_list, listener):
+    transformation_dict = {}
+    for i in range(len(transformation_list)):
+        if transformation_list[i] != '**' and transformation_list[i] != '':
+            try:
+                if transformation_list[i + 1] in ['', '**']:
+                    continue
+                transformation_dict[transformation_list[i + 1]] = get_single_transform(transformation_list[i], transformation_list[i + 1], listener)
+            except IndexError:
+                pass
     return transformation_dict
-                
-            
 
-def get_single_transform(parent_link,child_link,listener):
+
+def get_single_transform(parent_link, child_link, listener):
     try:
-        translation,rotation=listener.lookupTransform('/'+parent_link,'/'+child_link,rospy.Time(0))
-        rotation=tf.transformations.euler_from_quaternion(rotation)
-        transform=list(translation)+list(rotation)
+        translation, rotation = listener.lookupTransform(
+            '/' + parent_link, '/' + child_link, rospy.Time(0))
+        rotation = tf.transformations.euler_from_quaternion(rotation)
+        transform = list(translation) + list(rotation)
     except tf.LookupException:
-        transform=[0]*6
-    return map(round,transform,[4]*len(transform))
-    
+        transform = [0] * 6
+    return map(round, transform, [4] * len(transform))
 
 
 def settozero(d):
-    if not isinstance(d,dict):
-        print "Error: expected dict, got %s"%type(d)
+    if not isinstance(d, dict):
+        print "Error: expected dict, got %s" % type(d)
         return
-    output={}
-    for k,v in d.iteritems():
-        if isinstance(v,dict):
-            v=settozero(v)
-        elif isinstance(v,list):
-            v=[0]*len(v)
+    output = {}
+    for k, v in d.iteritems():
+        if isinstance(v, dict):
+            v = settozero(v)
+        elif isinstance(v, list):
+            v = [0] * len(v)
         else:
-            v=0
-        output[k]=v
+            v = 0
+        output[k] = v
     return output
-        
-    
-    
-            
+
+
 def __main__():
     rospy.init_node('cob_robot_calibration_generate_cal')
-    listener=tf.TransformListener()
+    listener = tf.TransformListener()
     rospy.sleep(1)
-    sss=simple_script_server()
-    sss.move("head","back")
-    minimal_system=rospy.get_param('minimal_system',None)
-    sensors=rospy.get_param('sensors',None)
-    output_system=rospy.get_param('output_system',None)
-    free_system=rospy.get_param('free_system',None)
-    
-    z=yaml.load(open(sensors,'r'))
-    transformations, to_calib=get_chains(z)
+    sss = simple_script_server()
+    sss.move("head", "back")
+    minimal_system = rospy.get_param('minimal_system', None)
+    sensors = rospy.get_param('sensors', None)
+    output_system = rospy.get_param('output_system', None)
+    free_system = rospy.get_param('free_system', None)
+
+    z = yaml.load(open(sensors, 'r'))
+    transformations = get_chains(z)
     print z
-    transformation_dict= generate_transformation_dict(transformations,listener)
-    system=yaml.load(open(minimal_system,'r'))
-    
-    
-    
-    if system.has_key('transforms'):
-        
-        t=dict(transformation_dict,**system['transforms'])
+    transformation_dict = generate_transformation_dict(
+        transformations, listener)
+    print transformation_dict
+    system = yaml.load(open(minimal_system, 'r'))
+
+    if 'transforms' in system:
+
+        t = dict(transformation_dict, **system['transforms'])
     else:
-        t=transformation_dict
-    
-    system['checkerboards']=rospy.get_param('~checkerboards',None)
+        t = transformation_dict
+
+    system['checkerboards'] = rospy.get_param('~checkerboards', None)
     if system['checkerboards'] is None:
         print '[ERROR]: Parameter checkerboards not found. Make sure it is set and try again'
         return
-    system['transforms']=t
-    
-    
-    free=system.copy()
-    
-    
-    free=settozero(free)
-    
+    system['transforms'] = t
+
+    free = system.copy()
+
+    free = settozero(free)
+
     '''
     print yaml.dump(system)
-    
+
     print "Calibration transformation suggestions are %s"%to_calib
-    
+
     print yaml.dump(free)
     '''
-    with open(output_system,'w') as f:
+    with open(output_system, 'w') as f:
         f.write(yaml.dump(system))
-    
-    with open(free_system,'w') as f:
+
+    with open(free_system, 'w') as f:
         f.write(yaml.dump(free))
-    
-if __name__=="__main__":
+
+if __name__ == "__main__":
     __main__()
-
-
